cd6f3085c4ad1bec094f5f303b08799da51026a2
iamdanieljojokoomson <53906917+jojo-koomson@users.noreply.github.com>
2024-06-19 12:50:34 +0000

    Update wl whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,6 +1,7 @@
 // whitelist.js
 const whitelist = [
     'https://smfind.10.247.5.116.nip.io',
+    'http://localhost:5173',
     'http://localhost:3000',
     'https://www.google.com',
     'https://maps.googleapis.com/maps/api/mapsjs',
