5cfa7cc69fa8bb87dd415c440545bbb97915e440
SMARTINFRACO DEVS <157625837+SmartInfracoAPPs@users.noreply.github.com>
2024-06-26 13:54:28 +0000

    Update dbconfig.js

--- dbconfig.js
+++ dbconfig.js
@@ -1,26 +1,21 @@
-// dbconfig.js
 import mongoose from "mongoose";
 import express from "express";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 import bodyParser from "body-parser";
 
 dotenv.config();
+
 const dbconfig = async () => {
   const app = express();
- 
+
   app.use(bodyParser.json());
 
   // MongoDB connection
   try {
-    // mongodb://localhost:27017/geoLTE
-    await mongoose.connect(`mongodb://root:y2IW91lqKRaZwtCXA0wzV6ZxQcIqvR2hdCdzVdTOByfbhjgyQ0c3r8Grlo307zax@10.247.5.115:5432/?directConnection=true`, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect('mongodb://root:tFhh2hQwHwKiIB7pwFhNfbylsafgxcfWOF9Yd532vcq4juGZFez0sQ02pa7abSMb@197.253.124.146:5432/?directConnection=true');
     console.log('MongoDB connected');
   } catch (error) {
     console.error('Error connecting to MongoDB:', error);
-    // Handle the error, maybe throw it or handle it according to your application's needs
     throw error;
   }
 
