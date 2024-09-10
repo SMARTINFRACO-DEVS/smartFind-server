d232cdad9259bc16dfa8be26190de74f6e0f58d8
iamdanieljojokoomson <53906917+jojo-koomson@users.noreply.github.com>
2024-09-10 14:28:39 +0000

    coolify

--- dbconfig.js
+++ dbconfig.js
@@ -13,8 +13,8 @@ const dbconfig = async () => {
   // MongoDB connection
   try {
     // mongodb://localhost:27017/geoLTE
-    await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@smart-server.fop0i76.mongodb.net/smartfind?ssl=true`, {
-    // await mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:5432/${process.env.MONGODB_CLUSTER}?directConnection=true`, {
+    // await mongoose.connect(`mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@smart-server.fop0i76.mongodb.net/smartfind?ssl=true`, {
+    await mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}:5432/${process.env.MONGODB_CLUSTER}?directConnection=true`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
